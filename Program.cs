97c8820a83ed1cdd051a9f191100655d38fb5613
Okera Johnson <okerajohnson@gmail.com>
2022-08-27 17:55:28 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium_Automation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new ChromeDriver("/Users/okerajohnson/Desktop/");
+
+			// This will open up the URL
+			driver.Url = "https://www.geeksforgeeks.org/";
+		}
+	}
+}
+
